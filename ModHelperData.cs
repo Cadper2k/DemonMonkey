82148444a66fe7ea5a9ccbd64ed5edc847accdf7
Cadper2k <Cadper2k@gmail.com>
2025-05-01 16:09:16 +0200

    Updated the info

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "DemonMonkey";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Adds the new Shinobi Monkey, heavily inspired by Naruto";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Cadper2k"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "DemonMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
